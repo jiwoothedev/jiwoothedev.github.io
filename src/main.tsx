fdccf48546c2af5f358c15c11176644b4db6e6b8
Jiwoo <jiwoothedev@gmail.com>
2025-08-22 23:57:41 +0900

    feat: react-router-dom 패키지 설치 및 라우팅 기본 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/common/Layout.tsx";
+import Home from "./pages/Home.tsx";
+import About from "./pages/About.tsx";
+import Blog from "./pages/Blog.tsx";
+import Project from "./pages/Project.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "about", element: <About /> },
+      { path: "blog", element: <Blog /> },
+      { path: "project", element: <Project /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      <img
+        src="https://images.unsplash.com/photo-1511044568932-338cba0ad803?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y2F0fGVufDB8fDB8fHww"
+        width={500}
+        height={50}
+      />
+    </div>
+  );
+}
