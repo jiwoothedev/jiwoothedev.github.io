3d6616743b38f058aa4ac8a48cd11c32c344844e
Jiwoo <jiwoothedev@gmail.com>
2025-08-26 18:35:54 +0900

    style: Header 배경 그라데이션 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,32 +1,33 @@
 import { Link } from "react-router-dom";
+import "../../App.css";
 
 export default function Header() {
   return (
-    <header
-      className="fixed top-5 left-1/2 -translate-x-1/2 z-50 w-[850px] max-w-[calc(100%-24px)] rounded-full px-1 py-1 pr-6
-        bg-white/10 supports-[backdrop-filter]:bg-white/12 backdrop-blur-xl backdrop-saturate-100 border border-white/15 shadow-[0_8px_30px_rgba(0,0,0,0.35)] ring-1 ring-white/10"
-    >
+    <header className="header fixed top-4 left-1/2 -translate-x-1/2 z-50 w-[850px] max-w-[calc(100%-24px)] px-1 py-1 pr-6">
       <nav className="w-full flex items-center justify-between text-white ">
         <div className="flex items-center gap-10">
           <Link to="/">
             <img src="/images/avatar.png" className="w-13 h-13 rounded-full" />
           </Link>
 
-          <ul className="flex items-center gap-14 font-bebas-neue text-white text-[17px] font-bold tracking-wider">
+          <ul className="flex items-center gap-8 font-bebas-neue text-white text-[16px] mt-1 tracking-wide">
             <li className="hover:text-gray-300 transition-colors">
-              <Link to="/about">ABOUT</Link>
+              <Link to="/">Home</Link>
             </li>
             <li className="hover:text-gray-300 transition-colors">
-              <Link to="/project">PROJECTS</Link>
+              <Link to="/about">About</Link>
             </li>
             <li className="hover:text-gray-300 transition-colors">
-              <Link to="/blog">BLOG</Link>
+              <Link to="/project">Projects</Link>
+            </li>
+            <li className="hover:text-gray-300 transition-colors">
+              <Link to="/blog">Blog</Link>
             </li>
           </ul>
         </div>
 
         <div className="flex items-center justify-center gap-6">
-          <div className="w-px h-5 bg-gray-300 mr-2"></div>
+          <div className="w-px h-5 bg-white/15 mr-1"></div>
           <a
             href="https://github.com/jiwoothedev"
             target="_blank" // 새 탭에서 열기
